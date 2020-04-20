7a39d1ae4d9fbfcaba8fef25a8dfd7682c8ef21c
Konstantin Kondr <i@kondr1.ru>
2020-04-20 19:02:08 +0300

    npm start

--- src/index.js
+++ src/index.js
@@ -5,5 +5,8 @@ const bot = new VkBot(process.env.TOKEN)
 bot.command('/Углепластик охлади траханье', (ctx) => {
   ctx.reply('Траханье охлажденно')
 })
+bot.command('/start', (ctx) => {
+  ctx.reply('Привет')
+})
 
 bot.startPolling()
